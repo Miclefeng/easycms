ca945efc18f6454bf87266ce24e955502d442b18
zhangshangshang <zhangshangshang@modian.com>
2017-10-17 13:59:05 +0800

    format index.phtml

--- public/index.php
+++ public/index.php
@@ -5,7 +5,5 @@ define('B_STATIC_PATH','/statics/backend/');
 
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 
-error_reporting(E_ALL);
-echo 111;
 $application->bootstrap()->run();
 ?>
