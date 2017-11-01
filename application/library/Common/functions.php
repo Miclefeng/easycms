0dc562ebd7e615fcbb6a0265feeb0c6e2ab17c4c
zhangshangshang <zhangshangshang@modian.com>
2017-11-01 12:08:29 +0800

    上传设置

--- application/library/Common/functions.php
+++ application/library/Common/functions.php
@@ -258,11 +258,7 @@ function http_post($url, $param, $post_file = false)
     if (is_string($param) || $post_file) {
         $strPOST = $param;
     } else {
-        $aPOST = array();
-        foreach ($param as $key => $val) {
-            $aPOST[] = $key . "=" . urlencode($val);
-        }
-        $strPOST = join("&", $aPOST);
+        $strPOST = http_build_query($param);
     }
     curl_setopt($oCurl, CURLOPT_URL, $url);
     curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
