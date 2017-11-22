f95e95cbce2fa0374985b2230cfcf4e9242af3f3
zhangshangshang <zhangshangshang@modian.com>
2017-11-22 11:52:48 +0800

    登录页修改

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../'));
-define('STATIC_PATH',APPLICATION_PATH.'public/statics/');
+define('STATIC_PATH',APPLICATION_PATH.'/public/statics/');
 
 $application = new Yaf_Application( APPLICATION_PATH . '/conf/application.ini');
 
