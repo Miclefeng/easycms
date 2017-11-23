9620fc1e06783ecfc93425d21fa410af172a0462
zhangshangshang <zhangshangshang@modian.com>
2017-11-23 14:16:09 +0800

    login statics edit

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
 define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../'));
+define('UPLOAD_PATH',APPLICATION_PATH.'/public/statics/uploads/');
 define('STATIC_PATH','/statics');
 
 $application = new Yaf_Application( APPLICATION_PATH . '/conf/application.ini');
-
 $application->bootstrap()->run();
 ?>
