c4f72769fb09a44760c6c2265e3b6aa4cfd44214
zhangshangshang <zhangshangshang@modian.com>
2017-12-12 14:37:38 +0800

    upload edit

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../'));
-define('APP_PATH', realpath(dirname(__FILE__).'/../application/'));
+define('APP_PATH', realpath(dirname(__FILE__).'/../application'));
 define('UPLOAD_PATH',APPLICATION_PATH.'/public/statics/uploads/');
 define('STATIC_PATH','/statics');
 
