a5706f42c7491e5d73432c1bf743ac685b8c58ec
ZSShang <1280054628@qq.com>
2018-04-05 13:40:20 +0800

    edit

--- application/controllers/Comment.php
+++ application/controllers/Comment.php
@@ -63,7 +63,7 @@ class CommentController extends Yaf_Controller_Abstract
         $comment->username = 'miclefengzss2018';
         $comment->content = 'this is a insert test!';
         $comment->create();
-        var_dump($comment->get_last_insert_id());
+//        var_dump($comment->get_last_insert_id());
         return false;
     }
 
